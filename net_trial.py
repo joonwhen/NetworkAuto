247494e945cc3905cc2d0fadf6f35f04888f1187
Jun Wen Toh (NCS) <junwen.toh@ncs.com.sg>
2020-09-14 18:34:16 +0800

    Multi-Threading

--- net_trial.py
+++ net_trial.py
@@ -1,37 +1,27 @@
+#parallel_query.py
 from netmiko import ConnectHandler
 from datetime import datetime
-import netmiko
-
-hosts = ['10.1.1.2', '10.1.1.3']
-
+from threading import Thread
 startTime = datetime.now()
 
-for routers in hosts:
-    print("Connecting to Host: "+routers)
-    connection = {
-        'device_type': 'cisco_ios_telnet',
-        'host':   routers,
-        'password': 'cisco',
-        'secret': 'cisco'
-    }         
-    
-    commands = ["show ip int brief"]
-    net_connect = ConnectHandler(**connection)
-    if net_connect:
-        print("Connected.")
-        net_connect.enable()
-        for command in commands:
-            output = net_connect.send_command(command)
-            print(output)
-            print(" ")
+hosts = ['10.10.1.3', '10.10.1.7', '192.168.1.142']
+threads = []
 
-    else:
-        print("Unable to connect to target host.")
-    
-    print("=============================================================")
+def checkparallel(ip):
+    device = ConnectHandler(device_type='cisco_ios_telnet', ip=ip, password='cisco', secret = 'cisco')
+    output = device.send_command("show ip int brief")
+    print ("\nConfiguration for IP: %s is as follow: \n" % (ip))
+    print(output)
 
-endTime = datetime.now()
+for host in hosts:
+    ip = host
+    t = Thread(target=checkparallel, args= (ip,))
+    t.start()
+    threads.append(t)
 
-total_time = endTime - startTime
+#wait for all threads to completed
+for t in threads:
+    t.join()
 
-print("Total time taken was: "+str(total_time))
+print ("\nTotal execution time:")
+print(datetime.now() - startTime)
