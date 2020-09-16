a16e4eb500c17df326b4d902e5c3fefa7e2382ab
Jun Wen Toh (NCS) <junwen.toh@ncs.com.sg>
2020-09-16 16:27:58 +0800

    Fixed SQL Integration and Typo. Credentials are now obtained from SQL Database

--- net_trial.py
+++ net_trial.py
@@ -23,37 +23,77 @@ def user_input():
     if(user_selection == '2'):
         IP_Selection()
 
+# Strip to IP Address
 def obtain_ip(raw_ip):
     y = str(raw_ip)
     x = re.search("[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", y)
     return x.group()
 
+# Strip Credentials Details
+def params_strip(raw_data):
+    y = str(raw_data)
+    x = re.search('[A-Za-z0-9_]+', y)
+    return x.group()
+
 # Device Selection
 def Device_Selection():
     print("Enter Hardware Name: ")
-    Hardware_Name = input()
+    chosen_device = input()
     db_selector = network_db.cursor()
-    db_selector.execute("SELECT IP_Address FROM network_hardware WHERE Hardware_Name = Hardware_Name")
+
+    # Obtain IP Address
+    db_selector.execute("SELECT IP_Address FROM port_ip WHERE Hardware_Name = '"+chosen_device+"' GROUP BY Hardware_Name")
     db_result = db_selector.fetchone() # .fetchall()
     address = obtain_ip(db_result)
-    device_config(address)
+
+    # Obtain Credentials to Login
+    db_selector.execute("SELECT C.Device_Type FROM credentials as C JOIN port_ip as P WHERE C.Hardware_Name = P.Hardware_Name AND C.Port_No = P.Port_No AND P.IP_ADDRESS = '"+address+"'")
+    db_result = db_selector.fetchall()
+    connection_info = []
+    connection_info.append(params_strip(db_result))
+
+    db_selector.execute("SELECT C.Device_Password FROM credentials as C JOIN port_ip as P WHERE C.Hardware_Name = P.Hardware_Name AND C.Port_No = P.Port_No AND P.IP_ADDRESS = '"+address+"'")
+    db_result = db_selector.fetchall()
+    connection_info.append(params_strip(db_result))
+
+    db_selector.execute("SELECT C.Secret FROM credentials as C JOIN port_ip as P WHERE C.Hardware_Name = P.Hardware_Name AND C.Port_No = P.Port_No AND P.IP_ADDRESS = '"+address+"'")
+    db_result = db_selector.fetchall()
+    connection_info.append(params_strip(db_result))
+
+    device_config(address, connection_info)
 
 # IP Selection
 def IP_Selection():
     print("Enter IP Address: ")
-    IP_Address = input()
-    device_config(IP_Address)
+    address = input()
+
+    db_selector = network_db.cursor()
+    # Obtain Credentials to Login
+    db_selector.execute("SELECT C.Device_Type FROM credentials as C JOIN port_ip as P WHERE C.Hardware_Name = P.Hardware_Name AND C.Port_No = P.Port_No AND P.IP_ADDRESS = '"+address+"'")
+    db_result = db_selector.fetchall()
+    connection_info = []
+    connection_info.append(params_strip(db_result))
+
+    db_selector.execute("SELECT C.Device_Password FROM credentials as C JOIN port_ip as P WHERE C.Hardware_Name = P.Hardware_Name AND C.Port_No = P.Port_No AND P.IP_ADDRESS = '"+address+"'")
+    db_result = db_selector.fetchall()
+    connection_info.append(params_strip(db_result))
+
+    db_selector.execute("SELECT C.Secret FROM credentials as C JOIN port_ip as P WHERE C.Hardware_Name = P.Hardware_Name AND C.Port_No = P.Port_No AND P.IP_ADDRESS = '"+address+"'")
+    db_result = db_selector.fetchall()
+    connection_info.append(params_strip(db_result))
+
+    device_config(address, connection_info)
 
 # Configuration
-def device_config(IP_Address):
+def device_config(IP_Address, connection_info):
     
     # Infornation Needed to Connect to Device
     # Improve on this to extract such information from SQL
     connection = {
-        'device_type': 'cisco_ios_telnet',
+        'device_type': connection_info[0],
         'host':   IP_Address,
-        'password': 'cisco',
-        'secret': 'cisco'
+        'password': connection_info[1],
+        'secret': connection_info[2]
     }     
     print("Connecting to: " + IP_Address)
     net_connect = ConnectHandler(**connection)
@@ -90,9 +130,6 @@ def device_config(IP_Address):
                 
     else:
         print("Connection Failed")
-
-
-
 
 # Initialisation    
 user_input()
